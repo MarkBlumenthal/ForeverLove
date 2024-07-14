88d720459c730010403ab614d1ef96d46c3072ef
Mark Blumenthal <markblumenthal@live.com>
2024-07-14 10:59:36 +0300

    changes

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,5 +1,6 @@
 // frontend/src/components/Home.jsx
 // import React from 'react';
+// import { Link } from 'react-router-dom';
 // import './Home.css'; // Ensure the CSS file is imported
 
 // const Home = () => {
@@ -11,12 +12,28 @@
 //       <div className="description-container">
 //         <p>Step into my world</p>
 //       </div>
+//       <div className="nav-buttons">
+//         <Link to="/" className="nav-link">
+//           Home
+//         </Link>
+//         <Link to="/art" className="nav-link">
+//            Art
+//         </Link>
+//         <Link to="/sculptures" className="nav-link">
+//           Sculptures
+//         </Link>
+//         <Link to="/jewelry" className="nav-link">
+//           Jewelry
+//         </Link>
+//         <Link to="/plants" className="nav-link">
+//           Plants
+//         </Link>
+//       </div>
 //     </div>
 //   );
 // };
 
 // export default Home;
-
 
 
 
@@ -39,7 +56,7 @@ const Home = () => {
           Home
         </Link>
         <Link to="/art" className="nav-link">
-           Art
+          Art
         </Link>
         <Link to="/sculptures" className="nav-link">
           Sculptures
@@ -50,6 +67,32 @@ const Home = () => {
         <Link to="/plants" className="nav-link">
           Plants
         </Link>
+      </div>
+      <div className="about-section">
+        <img src="/Aviv.png" alt="Artist" className="artist-image" />
+        <div className="about-me">
+          <h2>About Me</h2>
+          <p>
+            This is the about me section where the artist can write something about themselves.<br></br>
+            It can be a few sentences or a longer paragraph, detailing their background,
+            their art,<br></br> and anything else they would like to share with visitors.
+          </p>
+          <p>
+            This is the about me section where the artist can write something about themselves.<br></br>
+            It can be a few sentences or a longer paragraph, detailing their background,
+            their art,<br></br> and anything else they would like to share with visitors.
+          </p>
+          <p>
+            This is the about me section where the artist can write something about themselves.<br></br>
+            It can be a few sentences or a longer paragraph, detailing their background,
+            their art,<br></br> and anything else they would like to share with visitors.
+          </p>
+          <p>
+            This is the about me section where the artist can write something about themselves.<br></br>
+            It can be a few sentences or a longer paragraph, detailing their background,
+            their art,<br></br> and anything else they would like to share with visitors.
+          </p>
+        </div>
       </div>
     </div>
   );
