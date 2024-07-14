2d4cf88751d66adfc739bd8300bf529471a4cec8
Mark Blumenthal <markblumenthal@live.com>
2024-07-14 11:37:27 +0300

    changes

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,70 +1,4 @@
 // frontend/src/components/Home.jsx
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import './Home.css'; // Ensure the CSS file is imported
-
-// const Home = () => {
-//   return (
-//     <div className="home">
-//       <div className="header-container">
-//         <h1>Aviv Blumenthal</h1>
-//       </div>
-//       <div className="description-container">
-//         <p>Step into my world</p>
-//       </div>
-//       <div className="nav-buttons">
-//         <Link to="/" className="nav-link">
-//           Home
-//         </Link>
-//         <Link to="/art" className="nav-link">
-//           Art
-//         </Link>
-//         <Link to="/sculptures" className="nav-link">
-//           Sculptures
-//         </Link>
-//         <Link to="/jewelry" className="nav-link">
-//           Jewelry
-//         </Link>
-//         <Link to="/plants" className="nav-link">
-//           Plants
-//         </Link>
-//       </div>
-//       <div className="about-section">
-//         <img src="/Aviv.png" alt="Artist" className="artist-image" />
-//         <div className="about-me">
-//           <h2>About Me</h2>
-//           <p>
-//             This is the about me section where the artist can write something about themselves.<br></br>
-//             It can be a few sentences or a longer paragraph, detailing their background,
-//             their art,<br></br> and anything else they would like to share with visitors.
-//           </p>
-//           <p>
-//             This is the about me section where the artist can write something about themselves.<br></br>
-//             It can be a few sentences or a longer paragraph, detailing their background,
-//             their art,<br></br> and anything else they would like to share with visitors.
-//           </p>
-//           <p>
-//             This is the about me section where the artist can write something about themselves.<br></br>
-//             It can be a few sentences or a longer paragraph, detailing their background,
-//             their art,<br></br> and anything else they would like to share with visitors.
-//           </p>
-//           <p>
-//             This is the about me section where the artist can write something about themselves.<br></br>
-//             It can be a few sentences or a longer paragraph, detailing their background,
-//             their art,<br></br> and anything else they would like to share with visitors.
-//           </p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Home;
-
-
-
-
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css'; // Ensure the CSS file is imported
@@ -121,6 +55,9 @@ const Home = () => {
           </p>
         </div>
       </div>
+      <div className="gallery-heading">
+        <h2>My Favourite Pieces</h2>
+      </div>
       <div className="gallery">
         <img src="/gallery image.jpg" alt="Gallery Image 1" className="gallery-image" />
         <img src="/gallery image.jpg" alt="Gallery Image 1" className="gallery-image" />
@@ -132,5 +69,3 @@ const Home = () => {
 };
 
 export default Home;
-
-
