a7df2393fa1e2924d08267b2d5e1d23a60761398
Mark Blumenthal <markblumenthal@live.com>
2024-07-14 10:29:39 +0300

    changes

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,5 +1,28 @@
 // frontend/src/components/Home.jsx
+// import React from 'react';
+// import './Home.css'; // Ensure the CSS file is imported
+
+// const Home = () => {
+//   return (
+//     <div className="home">
+//       <div className="header-container">
+//         <h1>Aviv Blumenthal</h1>
+//       </div>
+//       <div className="description-container">
+//         <p>Step into my world</p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Home;
+
+
+
+
+
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Home.css'; // Ensure the CSS file is imported
 
 const Home = () => {
@@ -11,10 +34,26 @@ const Home = () => {
       <div className="description-container">
         <p>Step into my world</p>
       </div>
+      <div className="nav-buttons">
+        <Link to="/" className="nav-link">
+          Home
+        </Link>
+        <Link to="/art" className="nav-link">
+           Art
+        </Link>
+        <Link to="/sculptures" className="nav-link">
+          Sculptures
+        </Link>
+        <Link to="/jewelry" className="nav-link">
+          Jewelry
+        </Link>
+        <Link to="/plants" className="nav-link">
+          Plants
+        </Link>
+      </div>
     </div>
   );
 };
 
 export default Home;
-
 
