a46eb729b671a4a4fb944eab23fb2cf73d12ea81
Mark Blumenthal <markblumenthal@live.com>
2024-07-22 23:32:12 +0300

    changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Ensure the dev server binds to 0.0.0.0
+    port: 3000 // Or any port you prefer
+  },
+  build: {
+    outDir: 'dist' // This is the default output directory
+  }
+});
